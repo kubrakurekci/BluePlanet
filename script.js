3fa30e6bcf415c9f9eef9175281cdb9e8fd457af
Kübra Kürekçi <kubraa.kurekci@gmail.com>
2025-06-30 16:23:35 +0300

    Güncellemeler

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
         gundemScroll.scrollBy({ left: -300, behavior: "smooth" });
     });
 });
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = scrollTop / docHeight;
+    const scale = 0.1 + scrollPercent + 0.5;
+    document.querySelectorAll(".growingIcon").forEach((icon) => {
+      icon.style.transform = `scale(${scale})`;
+    });
+  });
